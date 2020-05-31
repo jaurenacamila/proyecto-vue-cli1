abdca0ba8112f2f818d00f9d73d7052bb6e39d3d
Camila Jaurena <jaurenacamila@gmail.com>
2020-05-31 13:19:45 -0300

    se agregaron los componentes formulario y http

---
+++ src/components/Formulario/src/components/Formulario.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'src-components-formulario',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/Http.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <section class="src-components-http">
+    <h1>src-components-http Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-http',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-http {
+
+  }
+</style>
